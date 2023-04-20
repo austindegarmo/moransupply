e82b4e6abc2636acc8bb5c16c6237e2315d1b9b2
austin degarmo <austindegarmo@yahoo.com>
2023-04-19 21:05:51 -0700

    Testing new map functionality

--- googlemaps/maps.js
+++ googlemaps/maps.js
@@ -1,26 +1,58 @@
+// let map;
+
+// async function initMap() {
+//   // The location of the store
+//   const position = { lat: 37.828, lng: -122.260 };
+//   // Request needed libraries.
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+//   // The map, centered at Uluru
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: position,
+//     mapId: "DEMO_MAP_ID",
+//   });
+
+//   // The marker, positioned at Uluru
+//   const marker = new AdvancedMarkerView({
+//     map: map,
+//     position: position,
+//     title: "Moran Supply",
+//   });
+// }
+
+// initMap();
+
+
+
+
 let map;
 
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: 37.828, lng: -122.260 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.76, lng: -73.983 },
+    zoom: 15,
+    mapTypeId: "satellite",
+    heading: 90,
+    tilt: 45,
   });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+  // add listener to button
+  document.getElementById("rotate").addEventListener("click", autoRotate);
 }
 
-initMap();
+function rotate90() {
+  const heading = map.getHeading() || 0;
+
+  map.setHeading(heading + 90);
+}
+
+function autoRotate() {
+  // Determine if we're showing aerial imagery.
+  if (map.getTilt() !== 0) {
+    window.setInterval(rotate90, 3000);
+  }
+}
+
+window.initMap = initMap;
